e8c2c06fc63e4702e93d6501ed7c12b76623d499
liciascl <licia.sales@hotmail.com>
2020-09-04 16:28:40 -0300

    Imagem sem bg subindo

--- nobg.py
+++ nobg.py
@@ -14,13 +14,24 @@ try:
     response = requests.post(
         'https://api.remove.bg/v1.0/removebg',
         files={'image_file': open('/home/borg/Desktop/no-bg/ImagemComFundo.jpg', 'rb')},
-        data={'800': '800'},
+        data={'size': 'auto'},
         headers={'X-Api-Key': 'Z6oCZ7L15ervntLoxLWZJAaq'},
     )
     if response.status_code == requests.codes.ok:
         with open('ImagemSemFundo.png', 'wb') as out:
             out.write(response.content)
             print("Fundo Retirado com sucesso")
+            filename = 'ImagemSemFundo.png'
+            basewidth = 800
+            img = Image.open(filename)
+            wpercent = (basewidth/float(img.size[0]))
+            hsize = int((float(img.size[1])*float(wpercent)))
+            img = img.resize((basewidth,hsize), Image.ANTIALIAS)
+            img.save(filename) 
+            print("deixando com 800px")
+
+
+
             cp = cmd.run("git add .", check=True, shell=True)
             print("Imagem sem bg subindo")
             message = "Imagem sem bg subindo"
