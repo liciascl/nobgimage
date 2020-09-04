c80090455cd2850cbcd6bc1b07bb035a3fdd251b
liciascl <licia.sales@hotmail.com>
2020-09-04 15:59:52 -0300

    deixando os nomes bonitos

--- nobg.py
+++ nobg.py
@@ -5,17 +5,17 @@ import subprocess as cmd
 
 try:
     down = ""
-    down = cmd.run("wget https://raw.githubusercontent.com/liciascl/nobgimage/master/teste.jpg", check=True, shell=True)
+    down = cmd.run("wget https://raw.githubusercontent.com/liciascl/nobgimage/master/ImagemComFundo.jpg", check=True, shell=True)
     print("Download Realizado com Sucesso")
 
     response = requests.post(
         'https://api.remove.bg/v1.0/removebg',
-        files={'image_file': open('/home/borg/Desktop/no-bg/teste.jpg', 'rb')},
+        files={'image_file': open('/home/borg/Desktop/no-bg/ImagemComFundo.jpg', 'rb')},
         data={'size': 'auto'},
         headers={'X-Api-Key': 'Z6oCZ7L15ervntLoxLWZJAaq'},
     )
     if response.status_code == requests.codes.ok:
-        with open('no-bg.png', 'wb') as out:
+        with open('ImagemSemFundo.png', 'wb') as out:
             out.write(response.content)
             print("Fundo Retirado com sucesso")
             cp = cmd.run("git add .", check=True, shell=True)
