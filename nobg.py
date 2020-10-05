68ae6d0473ae19254a73fbd9d191c242097ad333
liciascl <licia.sales@hotmail.com>
2020-10-05 16:33:22 -0300

    Imagem sem bg subindo

--- nobg.py
+++ nobg.py
@@ -5,6 +5,13 @@ from PIL import Image
 import os, shutil
 import requests
 
+try:
+    print("Puxando novos arquivos")   
+    cp = cmd.run("git add .", check=True, shell=True)
+
+
+except:
+    print (cp)
 
 dir = os.getcwd()
 dir = dir + "/fotos_com_fundo/"
@@ -12,24 +19,27 @@ print (dir)
 for index, file in enumerate(os.listdir(dir)):
     #renomeia as imagens com fundo
     antiga=dir+file
-    nova_com_fundo=dir+"image"+str(index)+".png"
+    nova_com_fundo=dir+"image"+str(index)
     os.rename(antiga,nova_com_fundo)
-    print("{} renomeada para image{}.png".format(file,index))
-
-    print("tirando o fundo da imagem")
-    response = requests.post(
-    'https://api.remove.bg/v1.0/removebg',
-    files={'image_file': open(nova_com_fundo, 'rb')},
-    data={'size': 'auto'},
-    headers={'X-Api-Key': 'Z6oCZ7L15ervntLoxLWZJAaq'},
-)
+    print("{} renomeada para image{}".format(file,index))
+    try:
+        print("tirando o fundo da imagem")
+        response = requests.post(
+        'https://api.remove.bg/v1.0/removebg',
+        files={'image_file': open(nova_com_fundo, 'rb')},
+        data={'size': 'auto'},
+        headers={'X-Api-Key': 'Z6oCZ7L15ervntLoxLWZJAaq'},
+    )
+        print("status", response.status_code)
+    except:
+        print("erro", response.status_code)
     if response.status_code == requests.codes.ok:
         filename = '/home/borg/Desktop/no-bg/fotos_sem_fundo/SemFundo_{}.png'.format(index)
         with open(filename, 'wb') as out:
             out.write(response.content)
             print("Fundo Retirado com sucesso")
             print("deixando com 800px")
-            basewidth = 800
+            basewidth = 1000
             img = Image.open(filename)
             wpercent = (basewidth/float(img.size[0]))
             hsize = int((float(img.size[1])*float(wpercent)))
