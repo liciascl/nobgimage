3b6c5f939a93ce6670eb02133e220271449844c7
liciascl <licia.sales@hotmail.com>
2020-10-05 15:22:28 -0300

    aceitando N imagens

--- nobg.py
+++ nobg.py
@@ -1,49 +1,48 @@
 # Requires "requests" to be installed (see python-requests.org)
-import requests
+import sys
 import subprocess as cmd
 from PIL import Image
-import sys
+import os, shutil
+import requests
 
+#try:
+    #pega quantas imagens precisa tirar o fundo
 
+dir = os.getcwd()
+dir = dir + "/fotos_com_fundo/"
+print (dir)
+for index, file in enumerate(os.listdir(dir)):
+    #renomeia as imagens com fundo
+    antiga=dir+file
+    nova_com_fundo=dir+"image"+str(index)+".png"
+    os.rename(antiga,nova_com_fundo)
+    print("{} renomeada para image{}.png".format(file,index))
 
-try:
-    down = ""
-   # down = cmd.run("wget https://raw.githubusercontent.com/liciascl/nobgimage/master/ImagemComFundo.jpg", check=True, shell=True)
-    print("Download Realizado com Sucesso")
-
+    print("tirando o fundo da imagem")
     response = requests.post(
-        'https://api.remove.bg/v1.0/removebg',
-        files={'image_file': open('/home/borg/Desktop/no-bg/ImagemComFundo.jpg', 'rb')},
-        data={'size': 'auto'},
-        headers={'X-Api-Key': 'Z6oCZ7L15ervntLoxLWZJAaq'},
-    )
+    'https://api.remove.bg/v1.0/removebg',
+    files={'image_file': open(nova_com_fundo, 'rb')},
+    data={'size': 'auto'},
+    headers={'X-Api-Key': 'Z6oCZ7L15ervntLoxLWZJAaq'},
+)
     if response.status_code == requests.codes.ok:
-        with open('ImagemSemFundo.png', 'wb') as out:
+        filename = '/home/borg/Desktop/no-bg/fotos_sem_fundo/SemFundo_{}.png'.format(index)
+        with open(filename, 'wb') as out:
             out.write(response.content)
             print("Fundo Retirado com sucesso")
-            filename = 'ImagemSemFundo.png'
+            print("deixando com 800px")
             basewidth = 800
             img = Image.open(filename)
             wpercent = (basewidth/float(img.size[0]))
             hsize = int((float(img.size[1])*float(wpercent)))
             img = img.resize((basewidth,hsize), Image.ANTIALIAS)
             img.save(filename) 
-            print("deixando com 800px")
 
 
 
-            cp = cmd.run("git add .", check=True, shell=True)
-            print("Imagem sem bg subindo")
-            message = "Imagem sem bg subindo"
-            cp = cmd.run(f"git commit -m '{message}'", check=True, shell=True)
-            cp = cmd.run("git push -u origin master -f", check=True, shell=True)
-            
-    else:
-        print("Error:", response.status_code, response.text)
 
 
-except:
-    print("Deu errooo")
+
 
 
 
