0c9f1dcf0b6b1151ef7d17b5eb4568bc7db7b3c1
liciascl <licia.sales@hotmail.com>
2020-10-05 17:24:41 -0300

    Imagem sem bg subindo

--- nobg.py
+++ nobg.py
@@ -7,7 +7,7 @@ import requests
 
 try:
     print("Puxando novos arquivos")   
-    cp = cmd.run("git add .", check=True, shell=True)
+    cp = cmd.run("git pull", check=True, shell=True)
 
 
 except:
@@ -19,51 +19,48 @@ print (dir)
 for index, file in enumerate(os.listdir(dir)):
     #renomeia as imagens com fundo
     antiga=dir+file
-    nova=dir+"image0"+str(index)
+    nova=dir+"image"+str(index)+".png"
     if antiga == nova:
         print ("As fotos ja foram tratadas")
         exit
     
     else:
-        if antiga == "teste.md":
-            exit
-        else:
-            os.rename(antiga,nova)
-            print("{} renomeada para image{}".format(file,index))
-            try:
-                print("tirando o fundo da imagem")
-                response = requests.post(
-                'https://api.remove.bg/v1.0/removebg',
-                files={'image_file': open(nova, 'rb')},
-                data={'size': 'auto'},
-                headers={'X-Api-Key': 'Z6oCZ7L15ervntLoxLWZJAaq'},
-            )
-                print("status", response.status_code)
-            except:
-                print("erro", response.status_code)
-            if response.status_code == requests.codes.ok:
-                filename = '/home/borg/Desktop/nobgimage/fotos_sem_fundo/SemFundo_{}.png'.format(index)
-                with open(filename, 'wb') as out:
-                    out.write(response.content)
-                    print("Fundo Retirado com sucesso")
-                    print("deixando com 800px")
-                    basewidth = 800
-                    img = Image.open(filename)
-                    wpercent = (basewidth/float(img.size[0]))
-                    hsize = int((float(img.size[1])*float(wpercent)))
-                    img = img.resize((basewidth,hsize), Image.ANTIALIAS)
-                    img.save(filename) 
+        os.rename(antiga,nova)
+        print("{} renomeada para image{}".format(file,index))
+        try:
+            print("tirando o fundo da imagem")
+            response = requests.post(
+            'https://api.remove.bg/v1.0/removebg',
+            files={'image_file': open(nova, 'rb')},
+            data={'size': 'auto'},
+            headers={'X-Api-Key': 'Z6oCZ7L15ervntLoxLWZJAaq'},
+        )
+            print("status", response.status_code)
+        except:
+            print("erro", response.status_code)
+        if response.status_code == requests.codes.ok:
+            filename = '/home/borg/Desktop/nobgimage/fotos_sem_fundo/SemFundo_{}.png'.format(index)
+            with open(filename, 'wb') as out:
+                out.write(response.content)
+                print("Fundo Retirado com sucesso")
+                print("deixando com 800px")
+                basewidth = 800
+                img = Image.open(filename)
+                wpercent = (basewidth/float(img.size[0]))
+                hsize = int((float(img.size[1])*float(wpercent)))
+                img = img.resize((basewidth,hsize), Image.ANTIALIAS)
+                img.save(filename) 
 
-            try:
-                cp = cmd.run("rm -rf /home/borg/Desktop/nobgimage/fotos_com_fundo/image0*", check=True, shell=True)
-                cp = cmd.run("git add .", check=True, shell=True)
-                print("Imagem sem bg subindo")
-                message = "Imagem sem bg subindo"
-                cp = cmd.run(f"git commit -m '{message}'", check=True, shell=True)
-                cp = cmd.run("git push -u origin master -f", check=True, shell=True)
-                print (cp)
-            except:
-                print(cp)
+        try:
+            cp = cmd.run("rm -rf /home/borg/Desktop/nobgimage/fotos_com_fundo/*.png", check=True, shell=True)
+            cp = cmd.run("git add .", check=True, shell=True)
+            print("Imagem sem bg subindo")
+            message = "Imagem sem bg subindo"
+            cp = cmd.run(f"git commit -m '{message}'", check=True, shell=True)
+            cp = cmd.run("git push -u origin master -f", check=True, shell=True)
+            print (cp)
+        except:
+            print(cp)
 
 
 
